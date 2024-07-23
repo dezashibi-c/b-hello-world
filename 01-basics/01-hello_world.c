454251cb79b7b5ce4cbe836c8ffb3e8693b2cedb
Navid Dezashibi <dezashibi@gmail.com>
2024-07-23 12:15:12 +0400

    format improvement

--- 01-basics/01-hello_world.c
+++ 01-basics/01-hello_world.c
@@ -1,41 +1,40 @@
 // ***************************************************************************************
-//    Project: Anatomy of a small "c" program
+//    Project: Anatomy of a Small C Program
 //    File: 01-hello_world.c
 //    Date: 2024-07-22
 //    Author: Navid Dezashibi
 //    Contact: navid@dezashibi.com
 //    Website: https://www.dezashibi.com | https://github.com/dezashibi
 //    License:
-//     Please refer to the LICENSE file, repository or website for more information about
-//     the licensing of this work. If you have any questions or concerns,
-//     please feel free to contact me at the email address provided above.
+//     Please refer to the LICENSE file in the repository or website for more information
+//     about the licensing of this work. If you have any questions or concerns,
+//     feel free to contact me at the email address provided above.
 // ***************************************************************************************
-// *  Description: a minimal `c` program is consist of one `.c` file that must be compiled
-// *               using the C compiler such as `gcc`, `clang`, `zig cc`, `tcc`, etc.
-// *               to compiler the program type
-// *               `<path to your compiler> -o <filename.exe or .out> <file.c>
-// *               `gcc -o 01-hello_world.out 01-hello_world.c`
+// *  Description: A minimal C program consists of one `.c` file that must be compiled
+// *               using a C compiler such as `gcc`, `clang`, `zig cc`, `tcc`, etc.
+// *               To compile the program, type:
+// *               `<path to your compiler> -o <filename.exe or .out> <file.c>`
+// *               Example: `gcc -o 01-hello_world.out 01-hello_world.c`
 // ***************************************************************************************
 
 /**
- * What is include and what are `.h` files?
+ * What is `#include` and what are `.h` files?
  *
- * C compilation process consist of pre-processing->then compilation
- * Preprocessor of C acts like an automated copy/paster which replaces
- * texts in the current source file using what we call them `Macros`
+ * The C compilation process consists of pre-processing and then compilation.
+ * The preprocessor in C acts like an automated copy/paster which replaces
+ * text in the current source file using what we call `macros`.
  *
- * in the other hand we have the concept of "header" files. header files
- * are places which we can put things like Macros, Function definitions,
- * type declarations such as Enums and Structs.
- * based on your previous experiences you might already have guessed what
- * they are and what they do but if you don't no problem.
+ * We also have the concept of "header" files. Header files are where we can put
+ * things like macros, function declarations, and type declarations such as enums and structs.
+ * Based on your previous experience, you might already have a guess about what
+ * they are and what they do, but if you don't, no problem.
  *
- * to include a header file means to tell the preprocessor "hey grab the content
- * of this file and put it exactly right here where I am (the include command)"
+ * Including a header file means telling the preprocessor "Hey, grab the content
+ * of this file and put it exactly right here where the include command is."
  *
- * this means the contents of "stdio.h" will be replaced by the following line.
+ * This means the contents of "stdio.h" will be replaced by the following line.
  *
- * more on header files later
+ * More on header files later.
  */
 #include <stdio.h>
 
@@ -45,19 +44,17 @@
  *
  * @return int
  *
- * Main entry of a c program is "main" function, it can return nothing or an integer number.
- * 0 generally means program ran and exited successfully and other number such as 1 or -1 means
- * there have been problem when the program was running.
+ * The main entry point of a C program is the "main" function. It can return nothing (void) or an integer.
+ * Returning 0 generally means the program ran and exited successfully, while other numbers such as 1 or -1
+ * indicate there were problems during execution.
  */
 int main(void)
 {
-    // the declaration of this function is coming from the "stdio.h" above
-    // we use it to print out simple string of characters which will be followed
-    // by the new line character on the user's screen.
+    // The declaration of this function comes from the "stdio.h" header included above.
+    // We use it to print a simple string of characters followed by a newline character on the user's screen.
     puts("Yay, Hello World!");
 
-    // simply return 0 meaning it's all good and we're done
-    // note that we could've remove this line
-    // but it's a good practice to always follow this convention.
+    // Return 0, indicating that everything is good and we're done.
+    // Note that we could remove this line, but it's a good practice to always follow this convention.
     return 0;
 }
