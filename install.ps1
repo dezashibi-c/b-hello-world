4c97fb960920fea2d346783e660341f31e82591a
Navid Dezashibi <dezashibi@gmail.com>
2024-08-19 09:55:51 +0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,18 @@
+# ***************************************************************************************
+#    Project: W64DevKit Auto Installer Script
+#    File: install.ps1
+#    Date: 2024-08-18
+#    Author: Navid Dezashibi
+#    Contact: navid@dezashibi.com
+#    Website: https://www.dezashibi.com | https://github.com/dezashibi
+#    License:
+#     Please refer to the LICENSE file in the repository or website for more information
+#     about the licensing of this work. If you have any questions or concerns,
+#     please feel free to contact me at the email address provided above.
+# ***************************************************************************************
+# *  Description: refer to readme file
+# ***************************************************************************************
+
 $apiUrl = "https://api.github.com/repos/skeeto/w64devkit/releases/latest"
 
 # Get the latest release information
